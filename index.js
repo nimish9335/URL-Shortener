cd88b2bc5191dcfbccec6cd88aad62fce5d9c03b
Nimish <np4447292@gmail.comm>
2026-04-09 16:44:16 +0530

    change files name along also add some more backend things

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const URL = require('./models/URL_Schema');
-const staticRoutes = require('./routes/static_routes');
-const path=require('path');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connect");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
 
-const urlRoutes = require('./routes/url_routes');
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 const app = express();
+const PORT = 8001;
 
-mongoose.connect('mongodb://127.0.0.1:27017/urlshortener')
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+connectToMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
+);
 
-app.set('view engine', 'ejs');
-app.set('views', path.resolve('./views'));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/',staticRoutes);
+app.use(cookieParser());
 
-// Routes
-app.use('/url', urlRoutes);
-app.listen(5000, () => console.log('Server running on 5000'));
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
--- routes/url_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getURL,shortURL}=require("../collectors/url_controller");
-
-router.get('/:shortId', getURL);
-router.post('/', shortURL);
-
-module.exports = router;
