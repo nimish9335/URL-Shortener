d2afdb19c1dbb08d33d74ca8d3400c7f5157c0ae
Nimish <np4447292@gmail.com>
2026-04-10 12:10:21 +0530

    some change in files

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -4,7 +4,7 @@ const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  if (!req.user) return res.redirect("/login");
+  if (!req.user) return res.redirect("/signup");
   const allurls = await URL.find({ createdBy: req.user._id });
   return res.render("home", {
     urls: allurls,
@@ -12,10 +12,12 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/signup", (req, res) => {
+  if (req.user) return res.redirect("login");
   return res.render("signup");
 });
 
 router.get("/login", (req, res) => {
+  if(!req.user) return res.render("/");
   return res.render("login");
 });
 
